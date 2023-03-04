ab97f63052ca7961983896a24682f6c41b21a032
shiwani <shiwanipaunikar1@gmail.com>
2023-03-03 21:48:51 -0800

    Added:User Need to Enter Valied Email

--- src/simple_form.js
+++ src/simple_form.js
@@ -1,9 +1,18 @@
-const textError = document.querySelector(".text-error-name");
+const textErrorName = document.querySelector(".text-error-name");
+const textErrorEmail = document.querySelector(".text-error-email");
 
 document.getElementById("text").addEventListener("change",event=>{
     let nameRegex = RegExp('^[A-Z][a-z A-Z \\s]{2,}$')
     if (!nameRegex.test(event.target.value))
-        textError.textContent = "Name is incorrect"
+        textErrorName.textContent = "Name is incorrect"
     else
-      textError.textContent = ""
+      textErrorName.textContent = ""
+})
+
+document.getElementById("email").addEventListener("change",event=>{
+    let emailRegex = RegExp('^[A-Za-z0-9][A-Za-z0-9+-]*[.]?[A-Za-z0-9+-]+@[A-Za-z0-9][A-Za-z0-9+-]*(.[A-Za-z0-9]+)?.[A-Za-z]{2,6}$')
+    if (!emailRegex.test(event.target.value))
+        textErrorEmail.textContent = "Email is incorrect"
+    else
+        textErrorEmail.textContent = ""
 })
