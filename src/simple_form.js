67968b644ee0b670ef44615ed0a939cb40c1d51f
shiwani <shiwanipaunikar1@gmail.com>
2023-03-03 22:00:47 -0800

    Added:User Need to Enter Valied Telephone Number

--- src/simple_form.js
+++ src/simple_form.js
@@ -7,3 +7,19 @@ document.getElementById("text").addEventListener("change",event=>{
     else
       textError.textContent = ""
 })
+document.getElementById("email").addEventListener("change",event=>{
+    let emailRegex = RegExp('^[A-Za-z0-9][A-Za-z0-9+-]*[.]?[A-Za-z0-9+-]+@[A-Za-z0-9][A-Za-z0-9+-]*(.[A-Za-z0-9]+)?.[A-Za-z]{2,6}$')
+    if (!emailRegex.test(event.target.value))
+        textErrorEmail.textContent = "Email is incorrect"
+    else
+        textErrorEmail.textContent = ""
+})
+
+
+document.getElementById("tel").addEventListener("change",event=>{
+    let telRegex = RegExp('^[0-9]{2} [0-9]{10}$')
+    if (!telRegex.test(event.target.value))
+        textErrorTel.textContent = "Telphone number is incorrect"
+    else
+        textErrorTel.textContent = ""
+})
